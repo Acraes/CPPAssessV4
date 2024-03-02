5a8879a791c32387d0edf64a7daf777f9548267b
Acraes <ToxiiCxVortex232@gmail.com>
2024-03-02 14:53:48 +1000

    Added Comments and removed a small bit of code
    Commenting in explanations for the String Class
    
    Comments Added for main section.

--- Code/C++Assessment1V1.cpp
+++ Code/C++Assessment1V1.cpp
@@ -24,7 +24,7 @@ int DisplayString()
 	return 0;
 }
 */
-
+//Just main 
 int main()
 {
 	String str1;
@@ -47,27 +47,30 @@ int main()
 
 	//str1.WriteToConsole();
 	//return 0;
-
+	#pragma region
+	/// Append, to write a small bit of string at the back of the string. Since there isn't anything at this point it will just add them in.
 	str1.Append(" ");
-
+	///Here it will add World behind the space, leaving " World"
 	str1.Append("World");
 
 	//str1.ToLower();
 	//str1.ToUpper();
-
+	///Prepend then inputs a small section into the string like Append but instead at the front. With this making it "Hello World"
 	str1.Prepend("Hello");
-
+	#pragma endregion
 	//str1.ToLower();
 	//str1.ToUpper();
 
+	#pragma region
+	///Same thing mentioned above but on the second string
 	str2.Append(" ");
 
 	str2.Append("World");
 
 	str2.Prepend("Different");
+	#pragma endregion
 
-
-	
+	/// Checking if String 1 and String 2 are the same, we know it isn't but this verifies it.
 	if (str1.EqualTo(str2))
 	{
 		std::cout << "str1 is equal to str2" << std::endl;
@@ -76,7 +79,8 @@ int main()
 	{
 		std::cout << "str1 is not equal to str2" << std::endl;
 	}
-
+	#pragma region 
+	///this section is to check what character is at the Index listed. In this case 4, so "o". 0 = H, 1 = e, 2 = l e.t.c.
 	size_t IndexCheck = 4;
 	
 	char CharacterCheck = str1.CharacterAt(IndexCheck);
@@ -84,35 +88,36 @@ int main()
 	
 	std::cout << " " << std::endl;
 	str1.WriteToConsole();
+	#pragma endregion
 
+	#pragma region
+	/// This section of code was to test the "Find()".
+	String substr1("Hello");
+	String substr2("Earth");
 
-	// Test the Find function
-	String substring1("Hello");
-	String substring2("Earth");
+	size_t index1 = str1.Find(substr1);
+	size_t index2 = str1.Find(substr2);
 
-	size_t index1 = str1.Find(substring1);
-	size_t index2 = str1.Find(substring2);
-
-	
+	///Made some strings and indexes to scan the original str1 for the line of text
 
 	if (index1 != String::notFound) {
-		std::cout << "Substring '" << substring1.CStr() << "' found at index: " << index1 << std::endl;
+		std::cout << "Substring '" << substr1.CStr() << "' found at index: " << index1 << std::endl;
 	}
 	else {
-		std::cout << "Substring '" << substring1.CStr() << "' not found in the string." << std::endl;
+		std::cout << "Substring '" << substr1.CStr() << "' not found in the string." << std::endl;
 	}
 
 	if (index2 != String::notFound) {
-		std::cout << "Substring '" << substring2.CStr() << "' found at index: " << index2 << std::endl;
+		std::cout << "Substring '" << substr2.CStr() << "' found at index: " << index2 << std::endl;
 	}
 	else {
-		std::cout << "Substring '" << substring2.CStr() << "' not found in the string." << std::endl;
+		std::cout << "Substring '" << substr2.CStr() << "' not found in the string." << std::endl;
 	}
+	///This is the result output
+	#pragma endregion
 
-	
 
-
-
+	///ReadFromConsole taking whatever input is... Inputted into the console and then WriteToConsole writing the value to check it works.
 	_InputStr.ReadFromConsole();
 
 	_InputStr.WriteToConsole();
--- Code/String.cpp
+++ Code/String.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include "String.h"
-
-
+///Side note, I use variables with the '_' in a lot of these but basically not at all in the main program document.
+///I do this cause the original String.h given had those and i didn't really change it much cause i was lazy.
+///OCD kicks in and i decided to just keep it consistent between String.cpp and String.h
+///Forgot about this detail when working on the Main Document, at this point, far too late to change unless absolutely necessary. -_-
+#pragma String
 String::String()
 {
-	//Default
+	///Default Constructor
     data = new char[1];
     data[0] = '\0';
-    size = 0;
+    size = 0; /// size set to 0 indicating a empty string.
 
-
+    ///Initalizes empty string by allocating memory for one character and setting it to the null character.
+    
 
 }
 
 String::String(const char* _str)
 {
-    size = 0;
+    
+
+    ///Calculates the size of the input string by iterating through it until it reaches null.
+    ///Memory allocated for the string 'data = new char[size + 1];' and copies it over.
+    size = 0; 
     while (_str[size] != '\0')
     {
         ++size;
@@ -27,10 +35,13 @@ String::String(const char* _str)
         data[i] = _str[i];
     }
     data[size] = '\0';
+    ///Escape character set to null to ensure the string is deleted.
 }
-
 String::String(const String& _other) 
 {
+    ///Copy Constructor
+    ///Constructor creates a new string object by copying the content of another string object.
+    ///Characters from the other strings are then copied over and the last character is set to the null character.
     size = _other.size;
     data = new char[size + 1];
     for (size_t i = 0; i < size; ++i)
@@ -43,14 +54,16 @@ String::String(const String& _other)
 
 String::~String()
 {
+    ///Destructor
     delete[] data;
-
+    ///Frees dynamically allocated memory when an object of the String class goes out of range or is deleted
 
 }
-
+#pragma endString
+#pragma Check 
 size_t String::Length() const
 {
-    
+    ///Returns the length of the string, which is stored in the 'size' member variable.
     
     return size;
 
@@ -59,6 +72,9 @@ size_t String::Length() const
 
 char& String::CharacterAt(size_t _index)
 {
+    ///Performs a check to ensure that the index is within bounds before accessing the character.
+    ///if the Index is valid, it prints out the character at the specified index '_index' in the string.
+    ///If the Index is invalid(Out of range/scope), prints error message and returns a null character.
     if (_index < size)
     {
         std::cout << "Character at Index " << _index << ": " << data[_index] << std::endl;
@@ -74,6 +90,8 @@ char& String::CharacterAt(size_t _index)
 
 const char& String::CharacterAt(size_t _index) const 
 {
+    /// Overloaded version of CharacterAt function for const objects.
+    ///Same as 'CharacterAt' function but doesn't print anything other than the error message and a reference to the character at the specified index for consts'.
     if (_index < size)
     {
         return data[_index];
@@ -88,6 +106,10 @@ const char& String::CharacterAt(size_t _index) const
 
 bool String::EqualTo(const String& _other) const
 {
+    ///Appends another string to the current string.
+    ///Then updates based on whether or not the new string is the same as the old string.
+    ///Returns a true or false value depending on the outcome.
+
     if (size != _other.size)
     {
         return false;
@@ -105,6 +127,12 @@ bool String::EqualTo(const String& _other) const
 
 String& String::Append(const String& _str)
 {
+    ///Appends another string to the current string.
+    ///Copies the characters from the current string and then from the appended string into the new memory.
+    ///Updates the size and deallocates the old memory, assigning the new memory to 'data'.
+
+
+
     char* temp = new char[size + _str.size + 1];
 
     for (size_t i = 0; i < size; ++i)
@@ -130,6 +158,9 @@ String& String::Append(const String& _str)
 
 String& String::Prepend(const String& _str)
 {
+    ///Prepends another string to the current string.
+    ///Copies the characters from the appended string and then from the current string into the new memory.
+    ///Updates the size and deallocates the old memory, assigning the new memory to 'data'.
     char* temp = new char[size + _str.size + 1];
 
     for (size_t i = 0; i < _str.size; ++i)
@@ -153,14 +184,21 @@ String& String::Prepend(const String& _str)
 
     return *this;
 }
+#pragma endCheck
 
+#pragma Alter 
 const char* String::CStr() const
 {
+    ///Returns a pointer to the internal character array 'data' of the string.
+    ///Allows external code to access the string representation of the 'String' object
     return data;
 }
 
 String& String::ToLower()
 {
+    ///Converts all upper case letters into their lowercase versions.
+    ///Iterates through each character in the string and checks if it's a uppercase letter, then converts it if so.
+
     for (size_t i = 0; i < size; ++i)
     {
         if (data[i] >= 'A' && data[i] <= 'Z')
@@ -173,6 +211,8 @@ String& String::ToLower()
 
 String& String::ToUpper()
 {
+    ///Converts all lower case letters into their Uppercase versions.
+    ///Iterates through each character in the string and checks if it's a lowercase letter, then converts it if so.
     for (size_t i = 0; i < size; ++i)
     {
         if (data[i] >= 'a' && data[i] <= 'z')
@@ -187,6 +227,9 @@ String& String::ToUpper()
 
 size_t String::Find(const String& _str)
 {
+    ///Searches for the first occurrence of the substring within the string.
+    ///Uses nested loops to compare Characters and when a match is found, returns the index of the starting position of the substring.
+    ///If none is found, returns the const 'notFound' from the 'String' class.
     for (size_t i = 0; i <= size - _str.size; ++i)
     {
         bool match = true;
@@ -208,6 +251,10 @@ size_t String::Find(const String& _str)
 
 size_t String::Find(size_t _startIndex, const String& _str)
 {
+    ///Overloaded version of the 'Find' function.
+    ///Checks if _startIndex is within the valid range of the string.
+    ///If yes, searches for the substring from teh specified index onward. Pretty much the same as the regular 'Find' function.
+    ///Otherwise the same as the 'Find' Function
     if (_startIndex < size)
     {
         for (size_t i = _startIndex; i <= size - _str.size; ++i)
@@ -229,10 +276,17 @@ size_t String::Find(size_t _startIndex, const String& _str)
     }
     return notFound;
 }
+#pragma endAlter
 
+#pragma Console
 
 String& String::ReadFromConsole()
 {
+    ///Gets the line of input from the console and stores it in 'buffer'.
+    ///Existing data in the 'String' Object is deleted and the size is reset to 0.
+    ///Calculates the size of the input until the null character.
+    ///Null character added at the end and a reference to the modified 'String' object is returned.
+
     const size_t bufferSize = 256;
     char buffer[bufferSize];
     std::cin.getline(buffer, bufferSize);
@@ -261,38 +315,13 @@ String& String::ReadFromConsole()
 
 String& String::WriteToConsole()
 {
+    ///Writes the contents of the 'String' Object to the console.
     std::cout << data;
     return *this;
 }
 
+#pragma endConsole
 
-
-bool String::operator==(const String& _other) 
-{
-    return EqualTo(_other);
-}
-
-bool String::operator!=(const String& _other) {
-    return !EqualTo(_other);
-}
-
-String& String::operator=(const String& _str) {
-    if (this != &_str) {
-        delete[] data;
-        size = _str.size;
-        data = new char[size + 1];
-        for (size_t i = 0; i < size; ++i) {
-            data[i] = _str.data[i];
-        }
-        data[size] = '\0';
-    }
-    return *this;
-}
-
-char& String::operator[](size_t _index) {
-    return CharacterAt(_index);
-}
-
-const char& String::operator[](size_t _index) const {
-    return CharacterAt(_index);
-}
+///Previously had some code here from when i was working on this a while ago i think, during the period of time when it hadn't been covered and i was looking for how to do this. 
+/// (Didn't use ChatGPT or anything like that, just a bunch of code from different sources, with some things changed here and there).
+/// Saw it and went, "Huh, that's weird." Deleted it and moved on to comment sections.
